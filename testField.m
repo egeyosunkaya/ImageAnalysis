fa7c2cb2ddfa38c97e00b853b36f1da786777f28
Serhan Gürsoy <gursoyserhan@yandex.com>
2018-01-03 18:58:41 +0300

    Fix 1.1 - Added Cluster Mean Color, Label Numbers and Labels
    Changed labels. Added label numbers.mat. Added mean color clusterer

--- testField.m
+++ testField.m
@@ -1,5 +1,29 @@
 
-img = imread('bee.jpg');
-[labels, numlabels] = getSPLabels(img,500,1,10);%numlabels is the same as number of superpixels
-figure;
-imagesc(label2rgb(labels));
+NAME = '000355.jpg';
+LABEL     = "Labels/label_" + NAME + ".mat";
+LABEL_NUM = "Labels/label_number_" + NAME + ".mat";
+PATH_IMG = "Data/" + NAME;
+load(LABEL);
+load(LABEL_NUM);
+I = im2double(imread(PATH_IMG{1}));
+testRes = getLabelGraph(labels, numlabels);
+
+[result,pixCount] = findMeanColor(I,labels,numlabels);
+
+Iclone = I;
+[rC,cC, ~] = size(I);
+
+for r=1:rC
+    for c=1:cC
+        tmp = result{labels(r,c)+1};
+        Iclone(r,c,1) = tmp(1,1) / 256;
+        Iclone(r,c,2) = tmp(1,2) / 256;
+        Iclone(r,c,3) = tmp(1,3) / 256;
+    end
+end
+
+imshow(Iclone);
+% figure; plot(testRes,'NodeLabel', testRes.Nodes.Name);
+ 
+
+
